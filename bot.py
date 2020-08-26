6867680f8e063cb637bbd41385cc7c70f4c631eb
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2020-08-26 17:29:01 -0400

    add creds file

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import json
+import config
 from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('z9eG60WrWK4GWw5K3ibZ0QcMb-zlnzI4Axhq4u_wTQqq')
+authenticator = IAMAuthenticator(config.api_key)
 assistant = AssistantV2(
     version='2018-09-20',
     authenticator=authenticator)
@@ -12,7 +13,7 @@ assistant.set_service_url('https://api.us-east.assistant.watson.cloud.ibm.com')
 # Sessions
 #########################
 
-session = assistant.create_session("a1a77e9e-7878-4cec-8db7-fe34c3fb24c4").get_result()
+session = assistant.create_session(config.application_id).get_result()
 print("Bot: Hey, how can I help you?")
 
 while(True):
@@ -23,7 +24,7 @@ while(True):
 
     # call api
     response = assistant.message(
-        "a1a77e9e-7878-4cec-8db7-fe34c3fb24c4", session["session_id"],
+        config.application_id, session["session_id"],
         input={'text': question},
         context={
             'metadata': {
@@ -32,7 +33,7 @@ while(True):
         }).get_result()
     print("Bot: " + response["output"]["generic"][0]["text"])
 
-assistant.delete_session("a1a77e9e-7878-4cec-8db7-fe34c3fb24c4", session["session_id"]).get_result()
+assistant.delete_session(config.application_id, session["session_id"]).get_result()
 
 # logs = assistant.list_logs(
 #     "<YOUR ASSISTANT ID>"
